2c79ea29583d32130500dd503e7c24f6f165a73b
Bernardo Pombinho <bernardopombinho@.gmail.com>
2024-12-20 19:54:13 +0000

    general errors adapted to component

--- resources/views/pages/activity_types/create.blade.php
+++ resources/views/pages/activity_types/create.blade.php
@@ -24,18 +24,7 @@
                     </div>
 
                     <!-- Error Messages -->
-                    @if($errors->any())
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            <ul class="text-white">
-                                @foreach($errors->all() as $error)
-                                    <li><strong>Error!</strong> {{ $error }}</li>
-                                @endforeach
-                            </ul>
-
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                    aria-label="Close"></button>
-                        </div>
-                    @endif
+                    <x-general-errors />
 
                     <!-- Activity Type Information Card -->
                     <div class="card">
--- resources/views/pages/users/edit.blade.php
+++ resources/views/pages/users/edit.blade.php
@@ -35,16 +35,7 @@
                         </div>
 
                         <!-- Error Messages -->
-                        @if($errors->any())
-                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                <ul class="text-white">
-                                    @foreach($errors->all() as $error)
-                                        <li><strong>Error!</strong> {{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>
-                        @endif
+                        <x-general-errors />
 
                         <!-- User Information Card -->
                         <div class="card">
